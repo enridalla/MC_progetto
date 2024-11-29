c7332ed09ac7a86a0c2a8118f520feacbf5381cb
enridalla <enrico.dallastella@studenti.unimi.it>
2024-11-29 15:07:28 +0100

    Creata grafica tab e grafica menuList

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Provider as PaperProvider } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import MenuList from './MenuList'; // La lista dei menù
+import Profile from './Profile'; // Pagina vuota placeholder
+import OrderStatus from './OrderStatus'; // Pagina vuota placeholder
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
+
+              if (route.name === 'Menù') {
+                iconName = 'food';
+              } else if (route.name === 'Ordine') {
+                iconName = 'truck-delivery';
+              } else if (route.name === 'Profilo') {
+                iconName = 'account';
+              }
+
+              return <Icon name={iconName} color={color} size={size} />;
+            },
+            tabBarActiveTintColor: '#6200ee',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+          <Tab.Screen name="Menù" component={MenuList} />
+          <Tab.Screen name="Ordine" component={OrderStatus} />
+          <Tab.Screen name="Profilo" component={Profile} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
